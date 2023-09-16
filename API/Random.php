01f71a44dc84805102f4e2a2ec5e1a11c65e1229
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2023-09-16 13:53:35 +0800

    Update Random.php

--- API/Random.php
+++ API/Random.php
@@ -1,1 +1,13 @@
+<?php
+// 一言的API接口
+$url = "https://v1.hitokoto.cn/";
 
+// 发送HTTP请求，获取一言内容
+$response = file_get_contents($url);
+
+// 解析JSON格式的响应数据
+$data = json_decode($response, true);
+
+// 输出一言内容
+echo $data['hitokoto'];
+?>
